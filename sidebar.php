37019adc5b81218112bc29a15106354f826501ba
hazama16138 <like_apple0302@ezweb.ne.jp>
2018-08-23 20:54:55 +0900

    header.php、footer.php、sidebar.phpにフックポイントを作りhome.phpで読み込んだ

---
+++ sidebar.php
@@ -0,0 +1,21 @@
+<section class="contents-sub">
+	<nav>
+		<figure>
+			<a href="gallery.html">
+				<img src="images/btn_gallery.jpg" width="200" height="114" alt="作品紹介">
+			</a>
+		</figure>
+
+		<figure>
+			<a href="tickets.html">
+				<img src="images/btn_tickets.jpg" width="200" height="84" alt="前売り券申し込み">
+			</a>
+			<figcaption>
+				前売り券好評発売中
+			</figcaption>
+		</figure>
+	</nav>
+	<?php if(is_active_sidebar('sidebar-1')): ?>
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php endif; ?>
+</section>
