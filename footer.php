bcb60227b0818b94f58deb26ed162c3461e115b4
hazama16138 <like_apple0302@ezweb.ne.jp>
2018-08-28 21:04:47 +0900

    ロゴの表示とcssの読み込み

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@
 	});
 	$('#pageTop').pageTop({speed: 500});
 </script>
+<script src="<?php echo get_template_directory_uri();	 ?>/js/slideshow.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
--- home.php
+++ home.php
@@ -2,13 +2,13 @@
 <nav id="pickup_field">
 	<ul class="bxslider">
 		<li class="now">
-			<img src="images/pickup1.jpg" alt="「企画展覧会。日本X写真。開催期間2015年8月28日より東京、大阪、福岡、名古屋、仙台巡業」" id="pickup1">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/pickup1.jpg" alt="「企画展覧会。日本X写真。開催期間2015年8月28日より東京、大阪、福岡、名古屋、仙台巡業」" id="pickup1">
 		</li>
 		<li>
-			<img src="images/pickup2.jpg" alt="全国から集まった写真、約200点展示" id="pickup2">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/pickup2.jpg" alt="全国から集まった写真、約200点展示" id="pickup2">
 		</li>
 		<li>
-			<img src="images/pickup3.jpg" alt="壁に風景写真が3点掛けられている" id="pickup3">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/pickup3.jpg" alt="壁に風景写真が3点掛けられている" id="pickup3">
 		</li>
 	</ul>
 </nav>
